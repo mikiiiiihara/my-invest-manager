dbcbbedc105734182f893e2b007309cd3c31bb93
井原 樹/Miki Ihara <kimity0824@icloud.com>
2024-01-14 02:34:48 +0900

    Update header.tsx

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Nav } from "react-bootstrap";
 import { NAVIGATION_LIST } from "../../../constants/navigation";
 import SignOut from "../sign-out/signOut";
+import { Center } from "../center/center";
 
 type Props = {
   userName?: string;
@@ -12,57 +13,61 @@ type Props = {
 
 const HeaderComponent: FC<Props> = ({ userName }) => {
   return (
-    <div className={styles.header}>
-      {userName ? (
-        <div className={styles.myAccount}>
-          <p>{userName ?? ""} </p>
-          <br />
-          <SignOut />
-        </div>
-      ) : (
-        <></>
-      )}
-      <Nav
-        className={`${styles.navbar} navbar-expand-lg navbar-dark bg-dark d-none-tb`}
-      >
-        <div className="container-xl">
-          <Link href={NAVIGATION_LIST.TOP} className="navbar-brand float-left">
-            My US Stock Portfolio
-          </Link>
-          <ul className={`navbar-nav mr-auto float-left ${styles.subMenu}`}>
-            {/* <li className="nav-item">
+    <Center>
+      <div className={styles.header}>
+        {userName ? (
+          <div className={styles.myAccount}>
+            <p>{userName ?? ""} </p>
+            <br />
+            <SignOut />
+          </div>
+        ) : (
+          <></>
+        )}
+        <Nav
+          className={`${styles.navbar} navbar-expand-lg navbar-dark bg-dark d-none-tb`}
+        >
+          <div className="container-xl">
+            <Link
+              href={NAVIGATION_LIST.TOP}
+              className="navbar-brand float-left"
+            >
+              My US Stock Portfolio
+            </Link>
+            <ul className={`navbar-nav mr-auto float-left ${styles.subMenu}`}>
+              {/* <li className="nav-item">
               <Link href={NAVIGATION_LIST.DIVIDEND} className="nav-link">
                 Dividend
               </Link>
             </li> */}
-            <li className="nav-item">
-              <Link href={NAVIGATION_LIST.PORTFOLIO} className="nav-link">
-                Portfolio
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href={NAVIGATION_LIST.SECTOR} className="nav-link">
-                Sector
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </Nav>
-      <div className={styles.navSp}>
-        <div className={styles.navSpContent}>
-          <Link href={NAVIGATION_LIST.TOP}>
-            <div className={styles.navSpItem}>
-              <Image
-                src="/asset.png"
-                width={36}
-                height={36}
-                style={{ objectFit: "contain" }}
-                alt="logo"
-              />
-              <p>Top</p>
-            </div>
-          </Link>
-          {/* <Link href={NAVIGATION_LIST.DIVIDEND}>
+              <li className="nav-item">
+                <Link href={NAVIGATION_LIST.PORTFOLIO} className="nav-link">
+                  Portfolio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href={NAVIGATION_LIST.SECTOR} className="nav-link">
+                  Sector
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </Nav>
+        <div className={styles.navSp}>
+          <div className={styles.navSpContent}>
+            <Link href={NAVIGATION_LIST.TOP}>
+              <div className={styles.navSpItem}>
+                <Image
+                  src="/asset.png"
+                  width={36}
+                  height={36}
+                  style={{ objectFit: "contain" }}
+                  alt="logo"
+                />
+                <p>Top</p>
+              </div>
+            </Link>
+            {/* <Link href={NAVIGATION_LIST.DIVIDEND}>
             <div className={styles.navSpItem}>
               <Image
                 src="/dividend.png"
@@ -74,33 +79,34 @@ const HeaderComponent: FC<Props> = ({ userName }) => {
               <p>Dividend</p>
             </div>
           </Link> */}
-          <Link href={NAVIGATION_LIST.PORTFOLIO}>
-            <div className={styles.navSpItem}>
-              <Image
-                src="/portfolio.png"
-                width={36}
-                height={36}
-                style={{ objectFit: "contain" }}
-                alt="logo"
-              />
-              <p>Portfolio</p>
-            </div>
-          </Link>
-          <Link href={NAVIGATION_LIST.SECTOR}>
-            <div className={styles.navSpItem}>
-              <Image
-                src="/sector.png"
-                width={36}
-                height={36}
-                style={{ objectFit: "contain" }}
-                alt="logo"
-              />
-              <p>Sector</p>
-            </div>
-          </Link>
+            <Link href={NAVIGATION_LIST.PORTFOLIO}>
+              <div className={styles.navSpItem}>
+                <Image
+                  src="/portfolio.png"
+                  width={36}
+                  height={36}
+                  style={{ objectFit: "contain" }}
+                  alt="logo"
+                />
+                <p>Portfolio</p>
+              </div>
+            </Link>
+            <Link href={NAVIGATION_LIST.SECTOR}>
+              <div className={styles.navSpItem}>
+                <Image
+                  src="/sector.png"
+                  width={36}
+                  height={36}
+                  style={{ objectFit: "contain" }}
+                  alt="logo"
+                />
+                <p>Sector</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Center>
   );
 };
 
